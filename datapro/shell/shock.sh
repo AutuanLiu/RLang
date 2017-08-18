ed2a129c90d60420026c287ab89fe2681f9c560f
AutuanLiu <autuanliu@163.com>
2017-08-18 11:31:36 +0800

    add new file

---
+++ datapro/shell/shock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# author： AutuanLiu
+# date： 2017/08/08
+# info: based on Ubuntu Kylin 16.04LTS
+
+# 数据路径设置
+shellDir="/home/autuanliu/data/Shell"
+storgeDir="/work/shock"
+csvDir="/work/ppcs"
+
+# 将满足条件的文件复制到指定目录下
+cd $csvDir
+for i in $(cat $shellDir/endresultfile.csv)
+    do
+       cp $i $storgeDir  2> /dev/null
+    done
